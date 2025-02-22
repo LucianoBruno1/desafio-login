cb2f20448a3fe553e1a327db2fa554ca00142031
Luciano <luciaanobruno@gmail.com>
2025-02-22 19:55:55 -0300

    fixing errors

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -11,28 +13,42 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login.html')
+            return redirect('login')
         else:
             form = RegisterForm()
     return render(request, 'register.html',{'form':form})
 
 
+
+
+
 def LoginAction(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Usuário ou senha inválidos')
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, username=user.username, password=password)  # Autentica usando o username do usuário
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Email ou senha inválidos')
+        except User.DoesNotExist:
+            messages.error(request, 'Email ou senha inválidos')
+
     return render(request, 'login.html')
+
+
+
+
 
 
 def LogoutAction(request):
     logout(request)
     return redirect('login')
 
+@login_required
 def home(request):
     return render(request, 'home.html')
