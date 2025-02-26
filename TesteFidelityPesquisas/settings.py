8bec732d29e6e01dfdcdad124f1be9c8ca259304
Luciano <luciaanobruno@gmail.com>
2025-02-26 01:29:03 -0300

    deploy on render

--- TesteFidelityPesquisas/settings.py
+++ TesteFidelityPesquisas/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@nw8wk90c2mc5w50oxmyt^_g&@mvcb=ik_(_mo2v0rxob8+-u1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testefidelitypesquisas-2.onrender.com']
 
 
 # Application definition
